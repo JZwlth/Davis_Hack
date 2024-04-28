a008dd5ee8d84dfcf1cb099a951b0fcc8774df6b
Jonathan <j0n4than9100@gmail.com>
2024-04-28 01:17:49 -0700

    GPT Controller working

---
+++ facechat/src/gpt4Controller.js
@@ -0,0 +1,119 @@
+
+const fs = require('fs');
+const openai = require("openai");
+
+function convertImagesToBase64Deprecated(filePaths, callback) {
+    let encodedImages = [];
+    for (x in filePaths) {
+        fs.readFile(x, function(error, data) {
+            if (error) {
+                console.error('Error occurred while reading the file:', error);
+                return;
+            }
+            const base64String = Buffer.from(data).toString('base64');
+            encodedImages.push(base64String);
+        });
+    }
+
+    callback(encodedImages);
+}
+
+class Gpt4Controller {
+    #connector;
+    #coachPrompt;
+
+    static shared = new Gpt4Controller();
+
+    constructor() {
+        this.#connector = new openai.OpenAI({
+            // TODO: Take key from environment
+            // FOR NOW - Paste it in here
+            apiKey: ""
+        });
+
+        this.#coachPrompt = "You are a communication coach. Give speaking advice on the following set of images and text. The images were taken as the speaker was talking and the transcribed text is provided as well.";
+
+    }
+
+    getConnector() {
+        return this.#connector;
+    }
+
+    getPrompt() {
+        return this.#coachPrompt;
+    }
+
+    convertImagesToBase64(filePaths, callback) {
+        let encodedImages = [];
+        for (let x of filePaths) {
+            fs.readFile(x, function(error, data) {
+                if (error) {
+                    console.error('Error occurred while reading the file:', error);
+                    return;
+                }
+                const base64String = Buffer.from(data).toString('base64');
+                encodedImages.push(base64String);
+            });
+        }
+    
+        callback(encodedImages);
+    }
+
+    async transcribeAudio(audioFilePath) {
+        const transcription = await Gpt4Controller.shared.getConnector().audio.transcriptions.create({
+            file: fs.createReadStream(audioFilePath),
+            model: "whisper-1"
+        })
+
+        return transcription.text;
+    }
+
+    async sendAllToGPT(audioFilePath, encodedImages) {
+        let transcribedAudio =  await Gpt4Controller.shared.transcribeAudio(audioFilePath);
+
+        let messages = [
+            {
+                role: "user",
+                content: [
+                    {type: "text", text: Gpt4Controller.shared.getPrompt()},
+                    {type: "text", text: transcribedAudio}
+                ]
+            }
+        ];
+
+        for (let x of encodedImages) {
+            messages.at(0).content.push(
+                {
+                    type: "image_url",
+                    image_url: {
+                        "url": "data:image/jpeg;base64,"+x
+                    } 
+                }
+            );
+        }
+
+        try {
+            const completion = await Gpt4Controller.shared.getConnector().chat.completions.create({
+                messages: messages,
+                model: "gpt-4-turbo",
+            });
+            console.log(completion.choices[0])
+            return completion.choices[0];
+
+        } catch (e) {
+            console.log("An error occurred.");
+            console.log(e);
+        }
+    }
+
+    async getResponse(audioFilePath, imageFilePaths) {
+        this.convertImagesToBase64(imageFilePaths, async function(encodedImages) {
+            Gpt4Controller.shared.sendAllToGPT(audioFilePath, encodedImages);
+        }); 
+    }
+};
+
+let controller = Gpt4Controller.shared;
+let audioFilePath = './src/assets/SelfIntroJon.m4a';
+let imageFilePaths = ['./src/assets/JonSelfie.jpg'];
+controller.getResponse(audioFilePath, imageFilePaths);
